8257516b679e2e42c2814d5c483f3bc84d0ecb26
Russell Gordon <rgordon@rsgc.on.ca>
2017-04-25 07:08:56 -0400

    Initial Commit

---
+++ Calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Russell Gordon on 4/25/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
